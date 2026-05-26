5d346d78ebe282ed8f1957515073fd92da4e7acf
rhokeeb <rhokeebsanni@gmail.com>
2026-05-26 04:07:18 +0100

    minor changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,8 @@ import API from "../api/products";
 const Home = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [skus, setSkus] = useState([]);
-  const [grossTotal, setGrossTotal] = useState(0);
-  const [rebate, setRebate] = useState(0);
+  const [grossTotal, setGrossTotal] = useState("");
+  const [rebate, setRebate] = useState("");
   const [history, setHistory] = useState([]);
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
