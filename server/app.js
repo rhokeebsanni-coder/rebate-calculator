086e9246e1d9cce30311992518610fff93e2f4f7
rhokeeb <rhokeebsanni@gmail.com>
2026-05-26 02:44:35 +0100

    fix cors origin

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://rebate-calculator-amber.vercel.app/",
+      "https://rebate-calculator-amber.vercel.app",
     ],
     credentials: true,
   }),
